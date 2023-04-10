20564fc5ee0f2e0de30e0130723c09875cef748a
atdey <95206697+atdey@users.noreply.github.com>
2023-04-10 22:37:07 +0530

    Initialization (#1)

---
+++ src/test/java/com/javacapability/dbmigration/DbmigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacapability.dbmigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
