c1490ea3081bc600f5d61dfa0af84bedc72b9633
Atish Dey <atdey@deloitte.com>
2023-04-11 21:58:48 +0530

    added flyway

--- src/main/java/com/javacapability/dbmigration/config/AppConfig.java
+++
@@ -1,7 +0,0 @@
-package com.javacapability.dbmigration.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-}
--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into users values (1, 'Atish', 1);
-insert into users values (2, 'Swapnil', 1);
-insert into users values (3, 'Shashank', 0);
-insert into users values (4, 'Arvind', 0);
-insert into users values (5, 'Netrica', 1);
