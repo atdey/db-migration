3d9fa5bb2e7e4e226758a7bf2603353b4cd60b76
Atish Dey <atdey@deloitte.com>
2023-04-20 00:47:57 +0530

    Enhanced with examples

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # db-migration
-This is a demo project for database migration.
+Create an order management system's database with following tables : 
+1. person
+2. product
+3. order
+
+Now, migrate some existing data in the Db.
+
+Additional requirement :
+Can we only create person table and migrate data in first release and rest in the second release ?
--- src/main/resources/Schema.sql
+++ src/main/resources/Schema.sql
@@ -1,8 +1,34 @@
-drop table if exists USERS;
-
-create table USERS(
+create table if not exists PERSON
+(
   ID int not null,
   NAME varchar(100) not null,
   STATUS int,
   PRIMARY KEY ( ID )
 );
+
+CREATE TABLE if not exists products
+(
+    id integer NOT NULL,
+    name varchar(100) not null,
+    category varchar(100),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE if not exists orders
+(
+    id integer NOT NULL,
+    person_id integer NOT NULL,
+    product_id integer NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_orders_person FOREIGN KEY (person_id)
+        REFERENCES public.person (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_orders_product FOREIGN KEY (product_id)
+        REFERENCES public.products (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+
+
